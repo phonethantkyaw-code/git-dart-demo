63546daaefa88fd862222bc1469131d871dbfdab
saizawlinn <6631501138@gmail.com>
2025-08-26 22:10:12 +0700

    Show all expenses

--- bin/git_dart_demo.dart
+++ bin/git_dart_demo.dart
@@ -1,5 +1,34 @@
-import 'package:git_dart_demo/git_dart_demo.dart' as git_dart_demo;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${git_dart_demo.calculate()}!');
+
+void main() {
+  List expenses = [
+    {"id": 1, "item": "lunch", "paid": 70},
+    {"id": 2, "item": "coffee", "paid": 45},
+    {"id": 3, "item": "room rent", "paid": 1600},
+  ];
+
+
+  print('===== Expense tracking app ===== ');
+  print('1. Show all expenses');
+  print('2. Show total paid');
+  stdout.write('Enter 1 or 2...');
+  // get user input
+  int? choice = int.tryParse(stdin.readLineSync()!);
+  if (choice == null) {
+    print('Invalid input');
+  } else {
+    switch (choice) {
+      case 1:
+        // loop to show all expenses
+        for (Map expense in expenses) {
+          print('${expense["id"]} - ${expense["item"]} - ${expense["paid"]}');
+        }
+        break;
+      case 2:
+        break;
+      default:
+        print('Invalid input');
+    }
+  }
 }
